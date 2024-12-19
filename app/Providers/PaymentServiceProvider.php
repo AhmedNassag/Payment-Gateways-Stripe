730a60dc24a529267bdae45fc3094240c8d69ce5
Ahmed Nabil <ahmednassag@gmail.com>
2024-12-19 16:19:18 +0200

    first commit

---
+++ app/Providers/PaymentServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Interfaces\PaymentGatewayInterface;
+use App\Services\StripePaymentService;
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void {
+
+       $this->app->bind(PaymentGatewayInterface::class,StripePaymentService::class);
+
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
